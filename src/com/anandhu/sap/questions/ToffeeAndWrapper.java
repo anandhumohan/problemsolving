a5fb79a57c2fba2a2704045d95346a2a98b58c22
chaipoint <anandhumohan5@gmail.com>
2017-10-10 01:20:10 +0530

    added

--- src/com/anandhu/sap/questions/ToffeeAndWrapper.java
+++ src/com/anandhu/sap/questions/ToffeeAndWrapper.java
@@ -3,8 +3,32 @@ package com.anandhu.sap.questions;
 public class ToffeeAndWrapper {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// total money
+		int money = 10;
+		// price of one toffee
+		int price = 1;
+		// no of wrappers for getting one toffee
+		int noOfWrappers = 2;
 
+		System.out.println(getMaxtoffee(money, price, noOfWrappers));
+
+	}
+
+	private static int getMaxtoffee(int money, int price, int noOfWrappers) {
+
+		int initialTofee = money / price;
+		int totalToffee = initialTofee + getTotal(initialTofee, noOfWrappers);
+		return totalToffee;
+
+	}
+
+	private static int getTotal(int initialTofee, int noOfWrappers) {
+		if(initialTofee < noOfWrappers) {
+			return 0;
+		}
+		int toff = initialTofee / noOfWrappers;
+		int rem = initialTofee % noOfWrappers;
+		return toff + getTotal(toff+rem, noOfWrappers);
 	}
 
 }
