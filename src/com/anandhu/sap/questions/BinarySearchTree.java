99744edad0b7a3eb881f7ee192ce5bc50130586e
chaipoint <anandhumohan5@gmail.com>
2017-10-19 19:08:43 +0530

    added

---
+++ src/com/anandhu/sap/questions/BinarySearchTree.java
@@ -0,0 +1,17 @@
+package com.anandhu.sap.questions;
+
+public class BinarySearchTree {
+	private int data;
+	private BinarySearchTree left;
+	private BinarySearchTree right;
+
+	public BinarySearchTree(int data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+	public static void main(String[] args) {
+		
+	}
+
+}
